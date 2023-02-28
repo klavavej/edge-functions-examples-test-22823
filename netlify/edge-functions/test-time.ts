6751a3cfebbd205ae5e47f6a64732ce1cd29e4a5
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2023-02-28 15:53:11 -0800

    Create test-time.ts

---
+++ netlify/edge-functions/test-time.ts
@@ -0,0 +1,15 @@
+import { Config } from "https://edge.netlify.com"
+
+export default () => {
+	const body = new Date().toUTCString()
+	const headers = {
+		"cache-control": "public, s-maxage=60"
+	}
+
+	return new Response(body, { headers })
+}
+
+export const config: Config = () => ({
+	cache: "manual",
+	path: "/current-time"
+})
