69f12a329e28829c2b9366fb830502eaffef97d9
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2023-02-28 16:10:10 -0800

    Update test-time.ts

--- netlify/edge-functions/test-time.ts
+++ netlify/edge-functions/test-time.ts
@@ -1,3 +1,10 @@
-export default () => new Response("Hello world")
+export default () => {
+	const body = new Date().toUTCString()
+	const headers = {
+		"cache-control": "public, s-maxage=60"
+	}
+
+	return new Response(body, { headers })
+}
 
 export const config = { path: "/test" }
