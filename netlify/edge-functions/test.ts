66e5710937fbd0ec6af9430964d97af7e3e5e452
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2023-03-01 08:44:12 -0800

    Update test.ts

--- netlify/edge-functions/test.ts
+++ netlify/edge-functions/test.ts
@@ -3,5 +3,5 @@ import { Config } from "https://edge.netlify.com"
 export default async () => new Response("Hello, world!")
 
 export const config: Config = {
-  path: ["/products/*"]
+  path: ["/cats/*.html"]
 }
